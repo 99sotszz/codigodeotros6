09545cd31f5e476fd8c465222f8255101946fe4d
Luis Soto <luisgabrielszousz@outlook.com>
2023-02-24 09:13:43 -0700

    subiendo codigo

--- Codigo6.java
+++ Codigo6.java
@@ -1,23 +1,23 @@
 public class codigo6 {
-	  
-	    int[] n = int[20];
+	  public static void main (String[]args) // faltaba es el metodo static
+	    int[] n = new int[20]; // nuevo int?
 
-	    for (int i = 0; i < 20; i+) {
+	    for (int i = 0; i < 20; i++) { // faltaba un + de incremento de i
 	      n[i] = (int)(Math.random() * 381) + 20;
-	      System.print(n[i] + " ");
+	      System.out.println(n[i] + " "); // faltaba el .out y ln nomas por si acaso 
 	    }
 	    
 	    System.out.printl("\n¿Qué números quiere resaltar? ");
-	    System.out.print("(1 – los múltiplos de 5, 2 – los múltiplos de 7): ");
+	    System.out.println("(1 – los múltiplos de 5, 2 – los múltiplos de 7) "); // estan raros los 2 puntos los borré y el ln
 	    int opcion = Integer.parseInt(System.console().readLine();
 
 	    int multiplo = (opcion == 1) : 5 ? 7;
 
-	    foreach (char e : n) {
+	    foreach (char e : n) { // ese char de donde sale?
 	      if (e % multiplo == 0) {
 	        System.out.print("[" + e + "] ");
 	       else {
-	        System.in.print(e + " ");
+	        System.out.print(e + " ");//out en lugar de in
 	      }
 	    }
 	  
